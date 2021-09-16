a5b234908ec7349d19f3b9f4c7fb25f4ae75dbf4
akapochi <redstar.ihsohaka@gmail.com>
2021-09-16 16:17:20 +0000

    変な地名が来たとき用のコードのデバッグ4

--- index.js
+++ index.js
@@ -32,10 +32,10 @@ app.post('/webhook', line.middleware(config), (req, res) => {
 
 // event handler
 async function handleEvent(event) {
-  // if (event.type !== 'message' || event.message.type !== 'text') {
-  //   // ignore non-text-message event
-  //   return Promise.resolve(null);
-  // }
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
 
   // // create a echoing text message
   // const echo = { type: 'text', text: `「${event.message.text}」というメッセージを受け取りました` };
@@ -43,9 +43,6 @@ async function handleEvent(event) {
   // // use reply API
   // return client.replyMessage(event.replyToken, echo);
 
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    return Promise.resolve(null);
-  }
 
   // 「天気教えて」以外の場合は反応しない
   // if (event.message.text !== '天気教えて') {
@@ -218,18 +215,18 @@ async function handleEvent(event) {
       type: 'text',
       text: '「 地名 + の天気」と言ってね'
     });
-  } else {
-    // axiosを使って天気APIにアクセス
-
-    // const CITY_ID = `130010`; // 取得したい地域のIDを指定
-    const URL = `https://weather.tsukumijima.net/api/forecast?city=${CITY_ID}`;
-    const res = await axios.get(URL);
-    const pushText = res.data.description.bodyText;
-    return client.pushMessage(event.source.userId, {
-      type: 'text',
-      text: pushText,
-    });
   }
+  // axiosを使って天気APIにアクセス
+
+  // const CITY_ID = `130010`; // 取得したい地域のIDを指定
+  const URL = `https://weather.tsukumijima.net/api/forecast?city=${CITY_ID}`;
+  const res = await axios.get(URL);
+  const pushText = res.data.description.bodyText;
+  return client.pushMessage(event.source.userId, {
+    type: 'text',
+    text: pushText,
+  });
+
 }
 
 // listen on port
